48ee199635a245f2a4975357e51742365a3c75e1
faopaic <faopaic@gmail.com>
2025-06-25 14:31:12 +0900

    修正

--- a.java
+++ a.java
@@ -4,7 +4,7 @@ public class a {
         String catFact = WebApiFunctions.getCatFact();
         ;
         try {
-            String apiKey = "3b177dc2-f6b1-4808-8304-94786f62f920:fx"; // ←ここだけ書き換える
+            String apiKey = "3b177dc2-f6b1-4808-8304-94786f62f920:fx";
             String result = Translate.translateText(catFact, "JA", apiKey);
             System.err.println("【ネコ豆知識】" + result);
         } catch (Exception e) {
